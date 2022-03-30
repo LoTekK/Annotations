692496b4fe70adfe43c333e0398bc908a6f5f496
Teck Lee Tan <lotekk78@gmail.com>
2022-03-30 17:53:24 +0800

    Add cursor placement, improved overall handling

--- Editor/AnnotateOverlay.cs
+++ Editor/AnnotateOverlay.cs
@@ -6,60 +6,68 @@ using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-[Overlay(typeof(SceneView), "AnnotateOverlay", "Annotate")]
-public class AnnotateOverlay : Overlay
+namespace TeckArtist.Tools
 {
-    private AnnotateTool tool;
-    private VisualElement m_Panel;
-    private bool m_Drawing;
-
-    public override VisualElement CreatePanelContent()
+    [Overlay(typeof(SceneView), "AnnotateOverlay", "Annotate")]
+    public class AnnotateOverlay : Overlay
     {
-        UpdatePanel();
-        return m_Panel;
-    }
+        private AnnotateTool tool;
+        private VisualElement m_Panel;
+        private bool m_Drawing;
 
-    private void UpdatePanel()
-    {
-        if (m_Panel == null)
+        public override VisualElement CreatePanelContent()
         {
-            m_Panel = new VisualElement();
+            UpdatePanel();
+            return m_Panel;
         }
-    }
 
-    public void Init(AnnotateTool _tool)
-    {
-        UpdatePanel();
-        tool = _tool;
-        var so = new SerializedObject(tool);
-        var width = new Slider("Width")
+        private void UpdatePanel()
         {
-            // value = tool.Width,
-            lowValue = 0,
-            highValue = 1,
-            showInputField = true
-        };
-        width.BindProperty(so.FindProperty(nameof(tool.Width)));
-        m_Panel.Add(width);
-        var strokeColor = new ColorField("Stroke Color");
-        strokeColor.BindProperty(so.FindProperty(nameof(tool.StrokeColor)));
-        m_Panel.Add(strokeColor);
-        var planeColor = new ColorField("Plane Color");
-        planeColor.BindProperty(so.FindProperty(nameof(tool.PlaneColor)));
-        m_Panel.Add(planeColor);
-        var button = new Button(() => tool.Clear())
-        {
-            text = "Clear"
-        };
-        m_Panel.Add(button);
-        m_Panel.MarkDirtyRepaint();
-    }
+            if (m_Panel == null)
+            {
+                m_Panel = new VisualElement();
+            }
+        }
 
-    public void Teardown()
-    {
-        UpdatePanel();
-        tool = null;
-        m_Panel.Clear();
-        m_Panel.MarkDirtyRepaint();
+        public void Init(AnnotateTool _tool)
+        {
+            UpdatePanel();
+            tool = _tool;
+            var so = new SerializedObject(tool);
+            var width = new Slider("Width")
+            {
+                // value = tool.Width,
+                lowValue = 0,
+                highValue = 1,
+                showInputField = true
+            };
+            width.BindProperty(so.FindProperty(nameof(tool.Width)));
+            m_Panel.Add(width);
+            var strokeColor = new ColorField("Stroke Color");
+            strokeColor.BindProperty(so.FindProperty(nameof(tool.StrokeColor)));
+            m_Panel.Add(strokeColor);
+            var planeColor = new ColorField("Plane Color");
+            planeColor.BindProperty(so.FindProperty(nameof(tool.PlaneColor)));
+            m_Panel.Add(planeColor);
+            var button = new Button(() => tool.Clear())
+            {
+                text = "Clear Annotations"
+            };
+            m_Panel.Add(button);
+            var clearCursor = new Button(() => tool.ClearCursor())
+            {
+                text = "Clear Cursor"
+            };
+            m_Panel.Add(clearCursor);
+            m_Panel.MarkDirtyRepaint();
+        }
+
+        public void Teardown()
+        {
+            UpdatePanel();
+            tool = null;
+            m_Panel.Clear();
+            m_Panel.MarkDirtyRepaint();
+        }
     }
 }
--- Editor/AnnotateTool.cs
+++ Editor/AnnotateTool.cs
@@ -1,178 +1,285 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.EditorTools;
 using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
-[EditorTool("Annotate")]
-public class AnnotateTool : EditorTool
+namespace TeckArtist.Tools
 {
-    public float Width = 0.5f;
-    private float lastWidth;
-    private float ssWidth;
-    public Color StrokeColor = Color.cyan * 0.7f;
-    public Color PlaneColor = Color.cyan * 0.25f;
-    private Transform container;
-    private LineRenderer currentLine;
-    private LineRenderer[] lines;
-    private bool isDrawing;
-    private Plane plane;
-    private Ray ray;
-    private float d;
-    private Vector3 lastPos;
-    public override GUIContent toolbarIcon => EditorGUIUtility.IconContent("editicon.sml");
-
-    [Shortcut("Annotations/Annotate", typeof(SceneView), KeyCode.N)]
-    static void ToggleAnnotate(ShortcutArguments args)
+    [EditorTool("Annotate")]
+    public class AnnotateTool : EditorTool
     {
-        ToolManager.SetActiveTool<AnnotateTool>();
-    }
+        public float Width = 0.5f;
+        private float lastWidth;
+        private float ssWidth;
+        public Color StrokeColor = Color.cyan * 0.7f;
+        public Color PlaneColor = Color.cyan * 0.25f;
+        private Transform container;
+        private LineRenderer currentLine;
+        private LineRenderer[] lines;
+        private bool isDrawing;
+        private Plane plane;
+        private Ray ray;
+        private float d;
+        private Vector3 lastPos;
+        private Vector2 clickPos;
+        private Vector3 cursorPos;
+        private bool useCursor;
+        public override GUIContent toolbarIcon => EditorGUIUtility.IconContent("editicon.sml");
 
-    public override void OnToolGUI(EditorWindow window)
-    {
-        // base.OnToolGUI(window);
-        Event e = Event.current;
-        UpdateLineWidth();
-        // if (e.modifiers.HasFlag(EventModifiers.Alt)) return;
-        if (!e.shift || e.alt) return;
-
-        var view = window as SceneView;
-        HandleUtility.AddDefaultControl(-1);
-
-        plane.SetNormalAndPosition(-view.camera.transform.forward, view.pivot);
-        ssWidth = HandleUtility.GetHandleSize(view.pivot) * Mathf.Lerp(0.01f, 0.1f, Width);
-
-        ray = HandleUtility.GUIPointToWorldRay(Vector2.zero);
-        plane.Raycast(ray, out d);
-
-        var color = Handles.color;
-        var zTest = Handles.zTest;
-        Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
-        Handles.color = PlaneColor;
-        var v = ray.GetPoint(d) - view.pivot;
-        Handles.DrawSolidDisc(view.pivot, -view.camera.transform.forward, v.magnitude);
-
-        Handles.color = color;
-        Handles.zTest = zTest;
-        if (e.isMouse && e.button == 0)
+        [Shortcut("Annotations/Annotate", typeof(SceneView), KeyCode.D)]
+        static void ToggleAnnotate(ShortcutArguments args)
         {
-            switch (e.type)
+            ToolManager.SetActiveTool<AnnotateTool>();
+        }
+
+        public override void OnToolGUI(EditorWindow window)
+        {
+            // base.OnToolGUI(window);
+            Event e = Event.current;
+            UpdateLineWidth();
+
+            var view = window as SceneView;
+
+            if (useCursor)
             {
-                case EventType.MouseDown:
-                    isDrawing = true;
-                    ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
-                    plane.Raycast(ray, out d);
-                    lastPos = ray.GetPoint(d);
-                    e.Use();
-                    break;
-                case EventType.MouseUp:
-                    isDrawing = false;
-                    currentLine = null;
-                    e.Use();
-                    break;
-                case EventType.MouseDrag:
-                    if (isDrawing)
-                    {
+                var p = HandleUtility.WorldToGUIPoint(cursorPos);
+                if (!view.position.Contains(p + view.position.min))
+                {
+                    useCursor = false;
+                }
+            }
+
+            plane.SetNormalAndPosition(-view.camera.transform.forward, useCursor ? cursorPos : view.pivot);
+            ssWidth = HandleUtility.GetHandleSize(view.pivot) * Mathf.Lerp(0.01f, 0.1f, Width);
+
+            var color = Handles.color;
+            var zTest = Handles.zTest;
+            Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
+            Handles.color = PlaneColor;
+
+            ray = HandleUtility.GUIPointToWorldRay(Vector2.zero);
+            plane.Raycast(ray, out d);
+            var p0 = ray.GetPoint(d);
+            ray = HandleUtility.GUIPointToWorldRay(window.position.size);
+            plane.Raycast(ray, out d);
+            var p1 = ray.GetPoint(d);
+            var v = p1 - p0;
+            Handles.DrawSolidDisc(useCursor ? cursorPos : view.pivot, -view.camera.transform.forward, v.magnitude);
+
+            // Handles.color = Color.cyan * 0.5f;
+            Handles.color = Color.yellow;
+            Handles.SphereHandleCap(-1, useCursor ? cursorPos : view.pivot, Quaternion.identity, ssWidth * 2, EventType.Repaint);
+            Handles.zTest = UnityEngine.Rendering.CompareFunction.Always;
+            Handles.color = Color.white;
+            Handles.DrawWireDisc(useCursor ? cursorPos : view.pivot, -view.camera.transform.forward, ssWidth * 4);
+            Handles.color = Color.red;
+            Handles.DrawWireDisc(useCursor ? cursorPos : view.pivot, -view.camera.transform.forward, ssWidth * 3);
+            // Handles.DrawSolidDisc(useCursor ? cursorPos : view.pivot, -view.camera.transform.forward, ssWidth / 2);
+
+            Handles.color = color;
+            Handles.zTest = zTest;
+
+            if (e.modifiers.HasFlag(EventModifiers.Alt)) return;
+            // if (!e.shift || e.alt) return;
+            HandleUtility.AddDefaultControl(-1);
+
+            if (e.isMouse)
+            {
+                switch (e.type)
+                {
+                    case EventType.MouseDown:
                         ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
-                        plane.Raycast(ray, out d);
-                        var p = ray.GetPoint(d);
-                        if ((p - lastPos).sqrMagnitude > (ssWidth / 2) * (ssWidth / 2))
+                        if (e.button == 0)
                         {
-                            if (currentLine == null)
+                            isDrawing = true;
+                            plane.Raycast(ray, out d);
+                            lastPos = ray.GetPoint(d);
+                            clickPos = e.mousePosition;
+                            e.Use();
+                        }
+                        if (e.button == 1 && e.shift)
+                        {
+                            var go = HandleUtility.PickGameObject(e.mousePosition, false);
+                            if (go && go.TryGetComponent<MeshFilter>(out var mf))
                             {
-                                AddLine();
-                                currentLine.positionCount = 1;
-                                currentLine.SetPosition(0, lastPos);
+                                if (MeshRaycast.IntersectRayMesh(ray, mf, out var hit))
+                                {
+                                    // view.pivot = hit.point;
+                                    cursorPos = hit.point;
+                                    useCursor = true;
+                                }
                             }
-                            ++currentLine.positionCount;
-                            lastPos = p;
+                            e.Use();
                         }
-                        if (currentLine != null)
+                        break;
+                    case EventType.MouseUp:
+                        if (e.button == 0)
                         {
-                            currentLine.SetPosition(currentLine.positionCount - 1, p);
+                            isDrawing = false;
+                            currentLine = null;
+                            if ((e.mousePosition - clickPos).sqrMagnitude < 4)
+                            {
+                                Selection.objects = new UnityEngine.Object[] { HandleUtility.PickGameObject(e.mousePosition, false) };
+                            }
+                            e.Use();
                         }
-                    }
-                    e.Use();
-                    break;
+                        break;
+                    case EventType.MouseDrag:
+                        if (isDrawing)
+                        {
+                            ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+                            plane.Raycast(ray, out d);
+                            var p = ray.GetPoint(d);
+                            var l = ssWidth / 2;
+                            // l *= smoothing;
+                            if ((p - lastPos).sqrMagnitude > l * l)
+                            {
+                                if (currentLine == null)
+                                {
+                                    AddLine();
+                                    currentLine.positionCount = 1;
+                                    currentLine.SetPosition(0, lastPos);
+                                }
+                                ++currentLine.positionCount;
+                                lastPos = p;
+                            }
+                            if (currentLine != null)
+                            {
+                                currentLine.SetPosition(currentLine.positionCount - 1, p);
+                            }
+                            e.Use();
+                        }
+                        break;
+                }
             }
         }
-    }
 
-    public void UpdateLineWidth()
-    {
-        if (container == null)
+        public void ClearCursor()
         {
-            return;
+            cursorPos = Vector3.zero;
+            useCursor = false;
         }
-        foreach (var line in lines)
-        {
-            line.widthMultiplier = HandleUtility.GetHandleSize(line.bounds.center) * Mathf.Lerp(0.01f, 0.1f, Width);
-        }
-    }
 
-    private void AddLine()
-    {
-        if (container == null)
+        public void UpdateLineWidth()
         {
-            container = new GameObject("Annotation")
+            if (container == null)
+            {
+                return;
+            }
+            foreach (var line in lines)
+            {
+                line.widthMultiplier = HandleUtility.GetHandleSize(line.bounds.center) * Mathf.Lerp(0.01f, 0.1f, Width);
+            }
+        }
+
+        private void AddLine()
+        {
+            if (container == null)
+            {
+                container = new GameObject("Annotation")
+                {
+                    hideFlags = HideFlags.HideAndDontSave
+                }.transform;
+            }
+            var go = new GameObject()
             {
                 hideFlags = HideFlags.HideAndDontSave
-            }.transform;
+            };
+            currentLine = go.AddComponent<LineRenderer>();
+            currentLine.useWorldSpace = true;
+            currentLine.widthMultiplier = ssWidth;
+            currentLine.sharedMaterial = Resources.Load<Material>("M_Annotate");
+            currentLine.startColor = currentLine.endColor = StrokeColor;
+            currentLine.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+            currentLine.numCapVertices = 3;
+            // currentLine.widthCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.1f, 1), new Keyframe(0.9f, 1), new Keyframe(1, 0));
+            go.transform.SetParent(container);
+            Undo.RegisterCreatedObjectUndo(go, "Annotation");
+            lines = container.GetComponentsInChildren<LineRenderer>();
         }
-        var go = new GameObject()
+
+        public void Clear()
         {
-            hideFlags = HideFlags.HideAndDontSave
-        };
-        currentLine = go.AddComponent<LineRenderer>();
-        currentLine.useWorldSpace = true;
-        currentLine.widthMultiplier = ssWidth;
-        currentLine.sharedMaterial = Resources.Load<Material>("M_Annotate");
-        currentLine.startColor = currentLine.endColor = StrokeColor;
-        currentLine.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-        currentLine.numCapVertices = 3;
-        // currentLine.widthCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.1f, 1), new Keyframe(0.9f, 1), new Keyframe(1, 0));
-        go.transform.SetParent(container);
-        Undo.RegisterCreatedObjectUndo(go, "Annotation");
-        lines = container.GetComponentsInChildren<LineRenderer>();
+            if (container == null)
+            {
+                return;
+            }
+            Undo.DestroyObjectImmediate(container.gameObject);
+            Undo.SetCurrentGroupName("Clear Annotations");
+        }
+
+        private void OnUndoRedo()
+        {
+            lines = container?.GetComponentsInChildren<LineRenderer>();
+        }
+
+        public override void OnActivated()
+        {
+            // base.OnActivated();
+            // Debug.Log("Start annotating");
+            if (EditorWindow.GetWindow<SceneView>().TryGetOverlay("AnnotateOverlay", out var overlay))
+            {
+                overlay.displayed = true;
+                (overlay as AnnotateOverlay).Init(this);
+            }
+            Undo.undoRedoPerformed += OnUndoRedo;
+        }
+
+        public override void OnWillBeDeactivated()
+        {
+            // base.OnWillBeDeactivated();
+            // Debug.Log("Stop annotating");
+            if (EditorWindow.GetWindow<SceneView>().TryGetOverlay("AnnotateOverlay", out var overlay))
+            {
+                (overlay as AnnotateOverlay).Teardown();
+                overlay.displayed = false;
+            }
+            Undo.undoRedoPerformed -= OnUndoRedo;
+        }
     }
 
-    public void Clear()
+    static class MeshRaycast
     {
-        if (container == null)
-        {
-            return;
-        }
-        Undo.DestroyObjectImmediate(container.gameObject);
-        Undo.SetCurrentGroupName("Clear Annotations");
-    }
+        static Type type_HandleUtility;
+        static MethodInfo meth_IntersectRayMesh;
+        static bool init;
 
-    private void OnUndoRedo()
-    {
-        lines = container?.GetComponentsInChildren<LineRenderer>();
-    }
-
-    public override void OnActivated()
-    {
-        // base.OnActivated();
-        // Debug.Log("Start annotating");
-        if (EditorWindow.GetWindow<SceneView>().TryGetOverlay("AnnotateOverlay", out var overlay))
+        static MeshRaycast()
         {
-            overlay.displayed = true;
-            (overlay as AnnotateOverlay).Init(this);
-        }
-        Undo.undoRedoPerformed += OnUndoRedo;
-    }
+            if (init)
+            {
+                return;
+            }
+            var editorTypes = typeof(Editor).Assembly.GetTypes();
 
-    public override void OnWillBeDeactivated()
-    {
-        // base.OnWillBeDeactivated();
-        // Debug.Log("Stop annotating");
-        if (EditorWindow.GetWindow<SceneView>().TryGetOverlay("AnnotateOverlay", out var overlay))
-        {
-            (overlay as AnnotateOverlay).Teardown();
-            overlay.displayed = false;
+            type_HandleUtility = editorTypes.FirstOrDefault(t => t.Name == "HandleUtility");
+            meth_IntersectRayMesh = type_HandleUtility.GetMethod("IntersectRayMesh",
+                                                                  BindingFlags.Static | BindingFlags.NonPublic);
+            init = true;
         }
-        Undo.undoRedoPerformed -= OnUndoRedo;
+
+        //get a point from interected with any meshes in scene, based on mouse position.
+        //WE DON'T NOT NEED to have to have colliders ;)
+        //usually used in conjunction with  PickGameObject()
+        public static bool IntersectRayMesh(Ray ray, MeshFilter meshFilter, out RaycastHit hit)
+        {
+            return IntersectRayMesh(ray, meshFilter.sharedMesh, meshFilter.transform.localToWorldMatrix, out hit);
+        }
+
+        //get a point from interected with any meshes in scene, based on mouse position.
+        //WE DON'T NOT NEED to have to have colliders ;)
+        //usually used in conjunction with  PickGameObject()
+        public static bool IntersectRayMesh(Ray ray, Mesh mesh, Matrix4x4 matrix, out RaycastHit hit)
+        {
+            var parameters = new object[] { ray, mesh, matrix, null };
+            bool result = (bool)meth_IntersectRayMesh.Invoke(null, parameters);
+            hit = (RaycastHit)parameters[3];
+            return result;
+        }
     }
 }
